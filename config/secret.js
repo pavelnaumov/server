48887175a205d0956b6e09d5898cf4d6a6009eb7
Pavel Naumov <pavliknaumov@gmail.com>
2019-07-08 12:08:50 +0300

    feat: implement passport-google

--- config/secret.js
+++
@@ -1,7 +0,0 @@
-export default {
-    jwtSecret: 'which wristwatches are swiss wristwatches',
-    googleAuth: {
-        clientID: '125883071714-brm69814inr7sq269jql7nq3m3iiel8l.apps.googleusercontent.com',
-        clientSecret: 'slR7jRMgr9RuGbu70574I41q'
-    }
-}
